d969ad6da05b6e7d00cf37555e6e2931d73695ba
Judith <j.massa@lambdaforge.io>
2019-10-22 15:29:33 +0200

    Update import script

--- Android/import_assets.sh
+++ Android/import_assets.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-files="index.html tool.js tool.css settings.js mediaSources.js canvasStyle.js settingsScreen.js mappingScreen.js fabric-3.2.0.min.js jquery-1.12.4.min.js audio buttons images video"
-assetdir="MMEAndroid/app/src/main/assets/www"
+assetdir="MMEAndroid/app/src/main/assets"
 
-rm -r "$assetdir"
-mkdir "$assetdir"
-
-
-for f in $files;
-do
-    echo "Copying $f"
-    cp -r ../"$f" "$assetdir"
-done
+rm -r "$assetdir"/www
+cp -r ../www "$assetdir"
